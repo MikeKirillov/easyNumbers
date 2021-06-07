e0e5704e115ece29ce62d9a5fa5ff355d0b50ff9
michaelkirillov <kirilov.mikhail91@yandex.ru>
2021-06-07 21:03:27 +0300

    what a commit

--- src/JavaCore/Branch_Class.java
+++ src/JavaCore/Branch_Class.java
@@ -2,6 +2,6 @@ package JavaCore;
 
 public class Branch_Class {
     public static void main(String[] args) {
-        System.out.println("some classes gonna be merged");
+        System.out.println("some classes gonna be merged once again");
     }
 }
