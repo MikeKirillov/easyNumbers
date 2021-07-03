0886eca9350f35f5a4fde6f2dc1a1f6989fbcda6
michaelkirillov <kirilov.mikhail91@yandex.ru>
2021-07-03 18:50:26 +0300

    off

--- src/JavaCore/PracticeWeek02/Task06/Vector.java
+++ src/JavaCore/PracticeWeek02/Task06/Vector.java
@@ -7,7 +7,7 @@ public abstract class Vector {
         this.x1 = x1;
         this.y1 = y1;
         this.z1 = z1;
-        this.x2 = z2;
+        this.x2 = x2;
         this.y2 = y2;
         this.z2 = z2;
     }
